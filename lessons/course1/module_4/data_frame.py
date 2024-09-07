0313f8a7725730ee18b692365618ef598cc6a10c
OGDevLabs <oscargarcia@ogdevlabs.onmicrosoft.com>
2024-09-07 09:41:03 -0500

    removing not required code

--- lessons/course1/module_4/data_frame.py
+++ lessons/course1/module_4/data_frame.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import os
 
-
-file_path = os.path.join(os.path.sep,'files','titanic_train_copy.csv')
 file_path = '../../../files/titanic_train_copy.csv'
 with open(file_path,'r') as file:
     content = file.read()
